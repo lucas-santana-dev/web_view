fb6b4bd648ef69685fab023de30bfd531640295f
Lucas Santana <lucassantanaads@gmail.com>
2022-08-25 18:18:22 -0300

    Segunda versão Mudança no build.gradle alterando a minSdkVersion para 19 e incluindo no pubspec.yaml a dependencia webview:^3.0.4

---
+++ lib/homepage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+///Para usar essa biblioteca é necessario instalar no pubspec.yaml a biblioteca
+///webview_flutter: ^3.0.4
+import 'package:webview_flutter/webview_flutter.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+///Para poder funcionar no android foi necessário modificar o build.gradle no
+///caminho: android>app>build.gradle para minSdkVersion 19
+///Para funcionar no IOS tbm será necessario modificar uma configuração que
+///ainda não fiz.
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: WebView(
+        initialUrl: 'https://sistemassga.com.br',//pagina inicial que sera aberta no webview
+        javascriptMode: JavascriptMode.unrestricted,
+
+      ),
+    );
+  }
+}
